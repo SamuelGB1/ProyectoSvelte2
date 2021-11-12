e22ddc4c556617d1b5711e1539e5c620525ac55c
SamuelGB1 <samuelgilbarceloiherm.amoros@gmail.com>
2021-11-12 23:44:26 +0100

    Creación formulario de Usuarios, se sincroniza correctamente con el formulario de roles

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,38 @@
 <script>
+
+let user = {
+		id: "",
+		name:  "",
+		surname: "",
+		adress: "",
+		phone: "",
+		email: "",
+		rol: ""
+	};
+
+	let listado_usuarios = [];
+
+	const guardarusers = () =>{
+		user = listado_usuarios.push(user);
+		console.log(user);
+        console.log(listado_usuarios);
+        resertformusers();
+
+	};
+
+    const resertformusers = () =>{
+        user = {
+            id: "",
+            name:  "",
+            surname: "",
+            adress: "",
+            phone: "",
+            email: "",
+			rol: ""
+	    }
+    }
+
+
 	 let rol = {
         rol_name: ""
     }
@@ -22,16 +56,73 @@
 </script>
 
 <main>
-	<form on:submit|preventDefault={guardarroles}>
-        <label for="namerol">Nombre del Rol: </label>
-        <input
-            bind:value={rol.rol_name}
-            type="text"
-            id="rol"
-            placeholder="Inserte el nombre del rol"
-        >
-        <button>
-            Guardar
-        </button>
-    </form>
+	<div class="container mx-auto">
+		<div class="grid grid-cols-2 gap-4">
+			<div class="p-4 shadow-md rounded-md text-lef">
+				<form on:submit|preventDefault={guardarusers}>
+					<label for="name">Nombre</label>
+					<input
+						bind:value={user.name}
+						id="name"
+						type="text"
+						placeholder="Inserte su nombre"
+					/>
+					<label for="surname">Apellidos</label>
+					<input
+						bind:value={user.surname}
+						id="username"
+						type="text"
+						placeholder="Inserte sus apellidos"
+					/>
+					<label for="adress">Dirección</label>
+					<input
+						bind:value={user.adress}
+						id="adress"
+						type="text"
+						placeholder="Inserte su dirección"
+					/>
+					<label for="phone">Teléfono</label>
+					<input
+						bind:value={user.phone}
+						id="phone"
+						type="text"
+						placeholder="Inserte su número de teléfono"
+					/>
+					<label for="email">Correo Electrónico</label>
+					<input
+						bind:value={user.email}
+						id="email"
+						type="text"
+						placeholder="Inserte su correo electrónico"
+					/>
+					<label for="rol">Rol</label>
+					<select bind:value={user.rol} id="rol">
+						{#each listado_roles as r}
+						<option value={r.rol_name}>
+							{r.rol_name}
+						</option>
+						{/each}
+					</select>
+					<button>
+						Guardar Usuario
+					</button>
+				</form>
+			</div>
+		</div>
+	</div>
+		<div class="p-4 shadow-md rounded-md text-center">
+			<form on:submit|preventDefault={guardarroles}>
+				<label for="namerol">Nombre del Rol: </label>
+				<input
+					bind:value={rol.rol_name}
+					type="text"
+					id="rol"
+					placeholder="Inserte el nombre del rol"
+				>
+				<button>
+					Guardar
+				</button>
+			</form>
+		</div>
+
 </main>
