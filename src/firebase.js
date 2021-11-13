08db02f2a1543adcce74795188c5e747893bd83c
SamuelGB1 <samuelgilbarceloiherm.amoros@gmail.com>
2021-11-13 20:09:52 +0100

    creación base de datos, falta configurarla

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBd__vT3GM1UtOl0_rOJS2Z0smRYHtXFNU",
+
+    authDomain: "proyectosvelte-4b051.firebaseapp.com",
+
+    projectId: "proyectosvelte-4b051",
+
+    storageBucket: "proyectosvelte-4b051.appspot.com",
+
+    messagingSenderId: "924866170090",
+
+    appId: "1:924866170090:web:dc426a31acb8169f089b93"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
